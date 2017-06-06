70bf024ba21f411d5f6fbeefc09abc199c8636eb
Colin Wren <colin.wren@bjss.com>
2017-06-06 13:09:08 +0100

    - Adding line endings - Commenting out redundant step implementation

--- steps/user_permissions.py
+++ steps/user_permissions.py
@@ -17,7 +17,6 @@ def user_with_role_logins(context, user_role):
         context.helpers.config.get('urls').get('login')
     login_page.driver.get(login_url)
     login_details = context.helpers.get_user_for_role(user_role)
-    context.username = login_details[0]
     login_page.login(*login_details)
 
 
@@ -32,22 +31,22 @@ def user_views_task_list(context):
     task_list_page.go_to_task_list()
 
 
-@then("they see a list of the tasks for the patients in the locations "
-      "they are assigned to")
-def list_of_tasks_for_patient_assigned_to(context):
-    """
-    Verify the patients in the task list are those the user is assigned to
-
-    :param context: Behave context
-    """
-    task_list_page = ListPage(context.driver)
-    patient_names = \
-        context.helpers.get_patient_names_for_user(context.username)
-    tasks = task_list_page.get_list_items()
-    for task in tasks:
-        patient_name = task_list_page.get_patient_from_item(task)
-        assert (patient_name in patient_names), \
-            "{} patient not in {}".format(patient_name, patient_names)
+# @then("they see a list of the tasks for the patients in the locations "
+#       "they are assigned to")
+# def list_of_tasks_for_patient_assigned_to(context):
+#     """
+#     Verify the patients in the task list are those the user is assigned to
+#
+#     :param context: Behave context
+#     """
+#     task_list_page = ListPage(context.driver)
+#     patient_names = \
+#         context.helpers.get_patient_names_for_user(context.username)
+#     tasks = task_list_page.get_list_items()
+#     for task in tasks:
+#         patient_name = task_list_page.get_patient_from_item(task)
+#         assert (patient_name in patient_names), \
+#             "{} patient not in {}".format(patient_name, patient_names)
 
 
 @then("the list of tasks is filtered to show tasks for the {user_role} role")
