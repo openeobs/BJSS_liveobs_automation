fe5f47ea02fae3a4c7e111b3d23a36addba7e4e8
Colin Wren <colin.wren@bjss.com>
2017-08-02 17:14:49 +0100

    - Starting to get SauceLabs running for this so we can get it running in CI

--- environment.py
+++ environment.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.remote.remote_connection import RemoteConnection
 from automation_helpers import AutomationHelpers
+from os import environ
 
 
 def before_all(context):
@@ -8,6 +11,40 @@ def before_all(context):
     
     :param context: Behave context 
     """
+    desired_caps = {
+        "platform": "Windows 10",
+        "browserName": "chrome",
+        "version": "58.0",
+
+    }
+    test_name = 'Testing Automation with Sauce Labs'
+    build_tag = environ.get('BUILD_TAG', None)
+    tunnel_id = environ.get('TUNNEL_IDENTIFIER', None)
+    username = environ.get('SAUCE_USERNAME', None)
+    access_key = environ.get('SAUCE_ACCESS_KEY', None)
+
+    selenium_endpoint = "https://%s:%s@ondemand.saucelabs.com:443/wd/hub" % (
+    username, access_key)
+    desired_caps['build'] = build_tag
+    desired_caps['tunnelIdentifier'] = tunnel_id
+    desired_caps['name'] = test_name
+
+    executor = RemoteConnection(selenium_endpoint, resolve_ip=False)
+    browser = webdriver.Remote(
+        command_executor=executor,
+        desired_capabilities=desired_caps
+    )
+
+    # This is specifically for SauceLabs plugin.
+    # In case test fails after selenium session creation having this
+    # here will help track it down.
+    # creates one file per test non ideal but xdist is awful
+    if browser is not None:
+        with open("%s.testlog" % browser.session_id, 'w') as f:
+            f.write("SauceOnDemandSessionID=%s job-name=%s\n" % (
+            browser.session_id, test_name))
+    else:
+        raise WebDriverException("Never created!")
     context.driver = webdriver.Chrome()
     context.helpers = AutomationHelpers('config.yml')
 
