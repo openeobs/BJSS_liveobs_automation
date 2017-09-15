e9838ec75e608600e8fab6a565b73801d11e0a88
Colin Wren <colin.wren@bjss.com>
2017-09-15 15:53:32 +0100

    - Disable linting rules cos yeah this file is massive

--- steps/background_setup.py
+++ steps/background_setup.py
@@ -1,52 +1,36 @@
+# pylint: disable=too-many-locals,no-member
+# pylint: disable=too-many-branches,too-many-statements
 """ Steps to use in the background set up of the features """
 from uuid import uuid4
 from behave import given
 
 
-@given('the user {user_name} exists with the {user_role} role')
-def ensure_user_with_role(context, user_name, user_role):
+def get_or_create_user(client, name, group_id, category_id):
     """
-    Do a search for user with the specified role or create one
-
-    :param context: Behave context
-    :param user_name: Name for the user
-    :param user_role: Role for the user
+    Do a search and find user, if no user then create them
+    :param client: ERPPeek Client
+    :param name: Name of the user to find
+    :param group_id: ID for user role
+    :param category_id: ID for user role
+    :return: user_id
     """
-    user_model = context.client.model('res.users')
-    group_model = context.client.model('res.groups')
-    group_search = group_model.search(
-        [
-            ['name', '=', 'NH Clinical {} Group'.format(user_role)]
-        ]
-    )
-    if not group_search:
-        raise Exception("No group {} found".format(user_role))
+    user_model = client.model('res.users')
     user_search = user_model.search(
         [
-            ['name', '=', user_name]
+            ['name', '=', name]
         ]
     )
     if user_search:
         found_user = user_model.read(user_search[0], ['login', 'groups_id'])
-        if not group_search[0] in found_user.get('groups_id'):
+        if not group_id in found_user.get('groups_id'):
             user_model.write(found_user.get('id'), {
                 'groups_id': [
-                    [6, 0, found_user.get('groups_id') + group_search]
+                    [6, 0, found_user.get('groups_id') + group_id]
                 ]
             })
-        context.helpers.user = found_user.get('login')
+        return found_user.get('login')
     else:
-        category_model = context.client.model('res.partner.category')
-        location_model = context.client.model('nh.clinical.location')
-        category_search = category_model.search(
-            [
-                ['name', '=', user_role]
-            ]
-        )
-        if category_search:
-            category_search = category_search[0]
-        else:
-            raise Exception("No category {} found".format(user_role))
+        location_model = client.model('nh.clinical.location')
         location_search = location_model.search(
             [
                 ['usage', '=', 'hospital']
@@ -59,19 +43,73 @@ def ensure_user_with_role(context, user_name, user_role):
                 pos_id.append(location['pos_id'][0])
         else:
             raise Exception("No hospital in system")
-        user_login = user_name.lower().replace(' ', '_').strip()
+        user_login = name.lower().replace(' ', '_').strip()
         user_model.create(
             {
-                'name': user_name,
+                'name': name,
                 'login': user_login,
                 'password': user_login,
-                'category_id': [[6, 0, [category_search]]],
-                'groups_id': [[6, 0, group_search]],
+                'category_id': [[6, 0, [category_id]]],
+                'groups_id': [[6, 0, group_id]],
                 'location_ids': [[6, 0, []]],
                 'pos_ids': [[6, 0, pos_id]]
             }
         )
-        context.helpers.user = user_login
+        return user_login
+
+
+def get_role_id_for_group(model, group):
+    """
+    Get the ID for the group
+    :param model: Group Model
+    :param group: Name of the group
+    :return: ID for the group
+    """
+    group_search = model.search(
+        [
+            ['name', '=', 'NH Clinical {} Group'.format(group)]
+        ]
+    )
+    if not group_search:
+        raise Exception("No group {} found".format(group))
+    return group_search
+
+
+def get_role_id_for_category(category_model, user_role):
+    """
+    Get the ID for the category
+    :param category_model: Category Model
+    :param user_role: Name of the category
+    :return: ID for the category
+    """
+    category_search = category_model.search(
+        [
+            ['name', '=', user_role]
+        ]
+    )
+    if category_search:
+        return category_search[0]
+    else:
+        raise Exception("No category {} found".format(user_role))
+
+
+@given('the user {user_name} exists with the {user_role} role')
+def ensure_user_with_role(context, user_name, user_role):
+    """
+    Do a search for user with the specified role or create one
+
+    :param context: Behave context
+    :param user_name: Name for the user
+    :param user_role: Role for the user
+    """
+
+    group_model = context.client.model('res.groups')
+    category_model = context.client.model('res.partner.category')
+    group_id = get_role_id_for_group(group_model, user_role)
+    category_id = get_role_id_for_category(category_model, user_role)
+    user_search = get_or_create_user(
+        context.client, user_name, group_id, category_id)
+    context.helpers.user = user_search
 
 
 @given("the patient {patient_name} is in {location} of {parent_location}")
