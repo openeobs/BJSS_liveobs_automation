0c68375a675a35e8ca9f8e228c7bd580dcc19203
Colin Wren <colin.wren@bjss.com>
2017-09-28 16:20:33 +0100

    - Until can parallelize automation will need to use headless chrome to carry out automation

--- environment.py
+++ environment.py
@@ -12,43 +12,46 @@ def get_browser():
     :return: Selenium driver
     """
     if environ.get('GO_PIPELINE_LABEL'):
-        desired_caps = {
-            "platform": "Windows 10",
-            "browserName": "chrome",
-            "version": "58.0",
-
-        }
-        test_name = 'Selenium for run {}'.format(
-            environ.get('GO_PIPELINE_LABEL', None))
-        build_tag = environ.get('GO_PIPELINE_LABEL', None)
-        tunnel_id = environ.get('GO_REVISION_LIVEOBS', None)
-        username = environ.get('SAUCELABS_USERNAME', None)
-        access_key = environ.get('SAUCELABS_ACCESS_TOKEN', None)
-
-        selenium_endpoint = \
-            "https://%s:%s@ondemand.saucelabs.com:443/wd/hub" % (
-                username, access_key)
-        desired_caps['build'] = build_tag
-        desired_caps['tunnelIdentifier'] = tunnel_id
-        desired_caps['name'] = test_name
-
-        executor = RemoteConnection(selenium_endpoint, resolve_ip=False)
-        browser = webdriver.Remote(
-            command_executor=executor,
-            desired_capabilities=desired_caps
-        )
-
-        # This is specifically for SauceLabs plugin.
-        # In case test fails after selenium session creation having this
-        # here will help track it down.
-        # creates one file per test non ideal but xdist is awful
-        if browser is not None:
-            with open("%s.testlog" % browser.session_id, 'w') as f:
-                f.write("SauceOnDemandSessionID=%s job-name=%s\n" % (
-                    browser.session_id, test_name))
-        else:
-            raise WebDriverException("Never created!")
-        return browser
+        #     desired_caps = {
+        #         "platform": "Windows 10",
+        #         "browserName": "chrome",
+        #         "version": "58.0",
+        #
+        #     }
+        #     test_name = 'Selenium for run {}'.format(
+        #         environ.get('GO_PIPELINE_LABEL', None))
+        #     build_tag = environ.get('GO_PIPELINE_LABEL', None)
+        #     tunnel_id = environ.get('GO_REVISION_LIVEOBS', None)
+        #     username = environ.get('SAUCELABS_USERNAME', None)
+        #     access_key = environ.get('SAUCELABS_ACCESS_TOKEN', None)
+        #
+        #     selenium_endpoint = \
+        #         "https://%s:%s@ondemand.saucelabs.com:443/wd/hub" % (
+        #             username, access_key)
+        #     desired_caps['build'] = build_tag
+        #     desired_caps['tunnelIdentifier'] = tunnel_id
+        #     desired_caps['name'] = test_name
+        #
+        #     executor = RemoteConnection(selenium_endpoint, resolve_ip=False)
+        #     browser = webdriver.Remote(
+        #         command_executor=executor,
+        #         desired_capabilities=desired_caps
+        #     )
+        #
+        #     # This is specifically for SauceLabs plugin.
+        #     # In case test fails after selenium session creation having this
+        #     # here will help track it down.
+        #     # creates one file per test non ideal but xdist is awful
+        #     if browser is not None:
+        #         with open("%s.testlog" % browser.session_id, 'w') as f:
+        #             f.write("SauceOnDemandSessionID=%s job-name=%s\n" % (
+        #                 browser.session_id, test_name))
+        #     else:
+        #         raise WebDriverException("Never created!")
+        #     return browser
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('headless')
+        return webdriver.Chrome(chrome_options=chrome_options)
     else:
         return webdriver.Chrome()
 
