258a0a1fc1c6267147b9112955c8f19a91c6135b
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-05-11 18:50:19 +0100

    Updated way of getting latest therapeutic level.

--- steps/therapeutic.py
+++ steps/therapeutic.py
@@ -542,12 +542,9 @@ def _get_current_therapeutic_obs_level_record(context, patient_name):
     :param patient_name:
     :return:
     """
-    level_model = context.client.model(
-        'nh.clinical.therapeutic.level'
-    )
-    domain = [('patient', '=', context.patients[patient_name].id)]
-    current_level_id = level_model.search(domain, order='id desc', limit=1)
-    current_level = level_model.browse(current_level_id[0])
+    patient = context.patients[patient_name]
+    level_model = context.client.model('nh.clinical.therapeutic.level')
+    current_level = level_model.get_current_level_record_for_patient(patient.id)
     return current_level
 
 
